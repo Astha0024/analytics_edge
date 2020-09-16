e5a7a9d3d76b741eedf611a34e546398fedc0da1
Astha0024 <puri0024@umn.edu>
2020-09-16 17:02:27 -0600

    Basic data analysis-v1

--- data_analysis.r
+++ data_analysis.r
@@ -1,7 +1,7 @@
 #Set working directory
 setwd("/Users/astpuri/Desktop/analytics_edge")
 
-#Load data file
+#Load data file. Data source is: https://apps.who.int/gho/data/node.main
 who <- read.csv("WHO.csv")
 
 #Get structure of data
@@ -11,4 +11,10 @@ str(who)
 max_literacy_rate_index <- which.max(who$LiteracyRate)
 cat(paste('','','Country with highest literacy rate is:', 
 who$Country[max_literacy_rate_index],'', sep="\n"))
+
+#Find the average child mortality rate of countries in each region
+cat("\n Child mortality rate of countries in each region: \n")
+print (
+    tapply(who$ChildMortality, who$Region, mean)
+)
 
