3e0b1e8e62f79ec61c0c1fe5983b0c253d1f7f5c
Astha0024 <puri0024@umn.edu>
2020-09-16 15:34:28 -0600

    Basic data analysis

--- data_analysis.r
+++ data_analysis.r
@@ -1,6 +1,14 @@
 #Set working directory
 setwd("/Users/astpuri/Desktop/analytics_edge")
 
+#Load data file
 who <- read.csv("WHO.csv")
+
+#Get structure of data
 str(who)
+
+#Find the coutry with highest literacy rate
+max_literacy_rate_index <- which.max(who$LiteracyRate)
+cat(paste('','','Country with highest literacy rate is:', 
+who$Country[max_literacy_rate_index],'', sep="\n"))
 
